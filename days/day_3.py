03d7728791161e2ff61c0eadc8689be6d9faf122
manuela.nenganga <manuela.nenganga@bitsighttech.com>
2025-02-14 14:39:06 +0000

    day_3

---
+++ days/day_3.py
@@ -0,0 +1,12 @@
+
+print(""" === The Ultimate Wacky Recipe Maker === """)
+print()
+food = input("Name a food: ")
+plant = input("Name a plant: ")
+cooking = input("Name a method of cooking: ")
+word = input("What's a word to describe burned food? ")
+item = input("Name a household item: ")
+
+print(" === Here is your personalised yummy recipe === ")
+print()
+print(cooking, food, "with", word, plant, " on a bed of ", item)
